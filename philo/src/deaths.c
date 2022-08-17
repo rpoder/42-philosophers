22490e78c202e9a4051294186e8d883b27cea826
Ronan PODER <rpoder@e1r9p6.clusters.42paris.fr>
2022-08-17 22:08:34 +0200

    ok

--- philo/src/deaths.c
+++ philo/src/deaths.c
@@ -6,7 +6,7 @@
 /*   By: rpoder <rpoder@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 11:51:34 by rpoder            #+#    #+#             */
-/*   Updated: 2022/08/17 21:26:34 by rpoder           ###   ########.fr       */
+/*   Updated: 2022/08/17 21:56:01 by rpoder           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,8 +63,8 @@ static bool	while_death(t_data *data, bool alive)
 
 void	*death_routine(void *arg)
 {
-	bool			alive;
-	t_data			*data;
+	bool	alive;
+	t_data	*data;
 
 	data = (t_data *)arg;
 	alive = true;
--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,28 +6,11 @@
 /*   By: rpoder <rpoder@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:38:55 by rpoder            #+#    #+#             */
-/*   Updated: 2022/08/17 19:38:39 by rpoder           ###   ########.fr       */
+/*   Updated: 2022/08/17 22:05:49 by rpoder           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-int	lauch_threads(t_data *data)
-{
-	struct timeval	now;
-	long long int	starting_at;
-	int				ret;
-
-	gettimeofday(&now, NULL);
-	starting_at = (now.tv_sec * 1000000) + now.tv_usec + 500000;
-	assign_philo_lastmeal(data, starting_at);
-	assign_philo_start(data, starting_at);
-	ret = pthread_create(&data->death_thread, NULL, &death_routine, data);
-	if (ret != 0)
-		return (-1);
-	pthread_mutex_unlock(&data->go_mutex);
-	return (0);
-}
 
 static int	has_eaten_enough(t_philo *philo)
 {
@@ -52,6 +35,13 @@ static void	pre_routine(t_philo *philo)
 		ft_usleep(philo->data, philo->data->t_eat / 4);
 }
 
+void static	sleep_and_think(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->finish_mutex);
+	sleeping(philo);
+	thinking(philo);
+}
+
 void	*routine(void *arg)
 {
 	t_philo	*philo;
@@ -68,31 +58,13 @@ void	*routine(void *arg)
 		{
 			pthread_mutex_lock(&philo->finish_mutex);
 			if (!first_time && !philo->finish)
-			{
-				sleeping(philo);
-				thinking(philo);
-			}
-			pthread_mutex_unlock(&philo->finish_mutex);
+				sleep_and_think(philo);
+			else
+				pthread_mutex_unlock(&philo->finish_mutex);
 			wait_and_take_forks(philo);
 			eating(philo);
 			first_time = false;
 		}
 	}
 	return (NULL);
-}
-
-int	join_threads(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->philo_nb)
-	{
-		if (pthread_join((data->philos[i].ptr), NULL) != 0)
-			return (0);
-		i++;
-	}
-	if (pthread_join((data->death_thread), NULL) != 0)
-		return (0);
-	return (1);
 }
